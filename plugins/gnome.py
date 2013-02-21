a98486440a6f0a5b461129eb755b77d46229697f
Lukas Nemec <lu.nemec@gmail.com>
2013-02-21 13:40:17 +0000

    fixed gnome3 plugin bug

--- plugins/gnome.py
+++ plugins/gnome.py
@@ -6,10 +6,12 @@ from subprocess import call
 
 image_alpha = True
 
+
 def set_wallpaper(image_file_with_path):
+
     filepath = os.path.abspath(image_file_with_path)
 
-    # TODO figure out how to test if wallpaper was set
-    call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', 'file://%s/%s' % (filepath, imgfile),])
+    # works on Gnome3
+    call(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', 'file://%s' % (filepath)])
 
     return True
