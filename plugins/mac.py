29d67a38f1470c5227431522da5c56305f0cca4b
Lukas Nemec <lu.nemec@gmail.com>
2013-02-21 13:16:10 +0000

    added support for macos

--- plugins/mac.py
+++ plugins/mac.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import os
+from appscript import app, mactypes
 from subprocess import call
 
 
 image_alpha = True
 
+
 def set_wallpaper(image_file_with_path):
+
     filepath = os.path.abspath(image_file_with_path)
 
-    # TODO fill code here, make it use mac python binding
-    #tell application "Finder"set desktop picture to {"Your HD:Users:YourUserName:desktops:" & situation & ".jpg"} as alias
-    #end tell
-    return True
+    try:
+
+        app('Finder').desktop_picture.set(mactypes.File(filepath))
+
+        return True
+
+    except Exception:
+
+        return False
